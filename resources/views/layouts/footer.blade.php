790dde209a04592af2bac8ef283ca1b65f18c072
EkoKun29 <ekoseptyonugroho9@.com>
2024-05-21 08:45:15 +0700

    first commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,43 @@
+<div class="container-fluid bg-dark text-white-50 footer pt-5 mt-5 wow fadeIn" data-wow-delay="0.1s">
+            <div class="container py-5">
+                <div class="row g-5">
+                    <div class="col-lg col-md-6">
+                        <h6 class="btn text-white mb-4" href="">PT ARINDO UTAMA PERKASA</h6>
+                        <p class="mb-2" style="font-size: 14px;">PT Arindo Utama Perkasa merupakan perusahaan yang bergerak
+                        di bidang pertanian. Pertama kali berdiri tahun 2000 berbentuk PB (Produsen Benih), setelah beroperasi selama 3 tahun berubah menjadi CV pada tahun 2003, 
+                        empat tahun kemudian menjadi PT. ARINDO UTAMA PERKASA mengawali usaha sebagai penyedia benih padi dengan merk Cap Bintang. Selama hampir 10 tahun, 
+                        merk ini telah menjadi andalan para petani untuk kebutuhan benih padi bagi lahan pertanian.
+                        </p>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <h5 class="text-white mb-4">KANTOR PUSAT</h5>
+                        <p class="mb-2" style="font-size: 14px;"><i class="fa fa-map-marker-alt me-3"></i>Alamat: Jl. Jakenan Winong No.KM 03, Batur Lor, Sembaturagung, Kec. Jakenan, Kabupaten Pati, Jawa Tengah 59182</p>
+                        <p class="mb-2" style="font-size: 14px;"><i class="fa fa-phone-alt me-3"></i>Telepon: (0295) 4790061</p>
+                        <p class="mb-2" style="font-size: 14px;"><i class="fa fa-envelope me-3"></i>Email: marketing.arindoutama@gmail.com</p>
+                        <div class="d-flex pt-2">
+                            <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
+                            <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
+                            <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-youtube"></i></a>
+                            <a class="btn btn-outline-light btn-social" href="mailto:marketing.arindoutama@gmail.com"><i class="fa fa-envelope"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <h5 class="text-white mb-4">Newsletter</h5>
+                        <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
+                        <div class="position-relative mx-auto" style="max-width: 400px;">
+                            <input class="form-control bg-transparent w-100 py-3 ps-4 pe-5" type="text" placeholder="Your email">
+                            <button type="button" class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">SignUp</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="copyright">
+                    <div class="row">
+                        <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                            Hak Cipta &copy; 2024 <a class="border-bottom" href="#">PT. ARINDO UTAMA PERKASA</a>. All Right Reserved. 
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-expand-lg bg-white navbar-light shadow sticky-top p-0">
+            <a href="index.html" class="navbar-brand d-flex align-items-center text-center py-0 px-4 px-lg-5">
+                 <img src="{{ asset('assets/img/logo.png') }}" alt="Arindo Logo" class="img-circle" width="50px" height="auto" style="margin-right: 10px;">
+                <h6 class="m-0 text-primary">PT. ARINDO UTAMA PERKASA</h6>
+            </a>
+            <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <div class="navbar-nav ms-auto p-4 p-lg-0">
+                    <a href="index.html" class="nav-item nav-link active">Beranda</a>
+                    <a href="about.html" class="nav-item nav-link">Profile</a>
+                    <a href="about.html" class="nav-item nav-link">Produk</a>
+                    {{-- <div class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Produk</a>
+                        <div class="dropdown-menu rounded-0 m-0">
+                            <a href="job-list.html" class="dropdown-item">Job List</a>
+                            <a href="job-detail.html" class="dropdown-item">Job Detail</a>
+                        </div>
+                    </div> --}}
+                    <a href="about.html" class="nav-item nav-link">Cara Pemesanan</a>
+                    <a href="contact.html" class="nav-item nav-link">Kontak</a>
+                </div>
+                <a href="" class="btn btn-primary rounded-0 py-4 px-lg-5 d-none d-lg-block">Jadi Distributor<i class="fa fa-arrow-right ms-3"></i></a>
+            </div>
+        </nav>
